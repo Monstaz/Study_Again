a00984d7ab407875188e152edcbcf72879e17b4b
Dmitriy <34037717+Monstaz@users.noreply.github.com>
2022-11-02 11:49:49 +0300

    Added "Cash" and f-strings
    practice in f-strings

--- BlackJack.py
+++ BlackJack.py
@@ -20,6 +20,7 @@ def menu():
 
 
 def deck():
+    player_cash = 100
     num_decks = ''
 
     while num_decks <= '0' or num_decks >= '5':
@@ -30,16 +31,28 @@ def deck():
     random.shuffle(game_deck)
     random.shuffle(game_deck)
     cls()
-    game(game_deck)
+    game(game_deck, player_cash)
 
 
-def game(game_deck):
+def game(game_deck, player_cash):
     lose = 0
+    bet = 0
+
+    while True:
+        try:
+            bet = int(input("Choose the bet(From 5 to 20): "))
+            if 5 <= bet <= 20:
+                break
+            else:
+                cls()
+        except (TypeError, ValueError):
+            print("Enter valid bet!")
+            continue
 
     dealer_hand = game_deck.pop(0)
-    print("Dealer's open hand: ", dealer_hand)
+    print(f"Dealer's open hand:{dealer_hand}")
     hand = game_deck[0] + game_deck[1]
-    print("Your cards:\n", game_deck[0], 'and', game_deck[1])
+    print(f"Your cards: {game_deck[0]} and {game_deck[1]}")
     del game_deck[0:2]
 
     if hand == 21:
@@ -47,7 +60,7 @@ def game(game_deck):
 
     elif hand == 22:
         hand = 12
-        print("Your points: ", hand)
+        print(f"Your points:{hand}")
 
     while True:
         action = input("1 - Hit\n2 - Stop\n")
@@ -56,13 +69,15 @@ def game(game_deck):
             if game_deck[0] == 11 and hand > 21:
                 hand -= 10
             if hand == 21:
-                print("Congrats! 21 points!\nWait dealer.")
+                print("Congrats! 21 points!\nWait dealer...")
                 break
+            player_info = f"Your card:{game_deck[0]}\nYour points:{hand}"
             if hand > 21:
-                print("Your card: ", game_deck[0], "\nYour points: ", hand, "\nYou got bust!")
+                player_cash -= bet
+                print(f"{player_info}You bust!\nYour cash is:{player_cas}")
                 lose = 1
                 break
-            print("Your card: ", game_deck[0], "\nNow your points: ", hand)
+            print(player_info)
             del game_deck[0]
         elif action == '2':
             break
@@ -72,31 +87,30 @@ def game(game_deck):
             dealer_hand += game_deck[0]
             if game_deck[0] == 11 and dealer_hand >= 21:
                 dealer_hand -= 10
-            print("Dealer's card:", game_deck[0], "\nDealer's score:", dealer_hand)
+            print(f"Dealer's card:{game_deck[0]}\nDealer's player_cash:{dealer_hand}")
             del game_deck[0]
-        result = "Your score:" + str(hand) + "\nDealer's score:" + str(dealer_hand)
+        result_info = f"Your player_cash:{hand}\nDealer's player_cash:{dealer_hand}"
         if hand < dealer_hand <= 21:
-            result += "\nYou lose!"
-            print(result)
+            player_cash -= bet
+            print(result_info + f"\nYou Lose!\nNow your cash:{player_cash}")
         elif dealer_hand == hand:
-            result += "\nDraw!"
-            print(result)
+            player_cash += bet
+            print(result_info + f"\nDraw!\nNow your cash:{player_cash}")
         elif dealer_hand < hand or dealer_hand > 21:
-            result += "\nYou win!"
-            print(result)
-    else:
-        print("You lose!")
+            player_cash += bet * 2
+            print(result_info + f"\nYou Win!\n Now your cash:{player_cash}")
 
-    retry = ''
-    while retry != 1 or retry != 2:
-        retry = input("Want to play again?\n1 - Yes\n2 - No(Back to menu)\n")
+    if player_cash <= 0:
+        input("You lose all of your cash!\n Type any button to continue")
+        menu()
+    while True:
+        retry = input("Want to play again?\n1 - Yes\n2 - Back to menu\n")
         if retry == '1':
             cls()
-            game(game_deck)
+            game(game_deck, player_cash)
         elif retry == '2':
+            input(f"Thanks for playing!\nYour cash is {player_cash}\nGood luck!\nPress any button to continue")
             menu()
-        else:
-            continue
 
 
 menu()
