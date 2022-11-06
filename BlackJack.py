f17dccbccbd7186e652b88923a62496927bcd0ab
Dmitriy Alekseev <monstaz33@gmail.com>
2022-11-06 15:34:38 +0300

    0.8
    Now programm save info(name and score) to .txt file, also can read it.

--- BlackJack.py
+++ BlackJack.py
@@ -1,26 +1,49 @@
+import json
 import random
 import os
 import sys
+import pathlib
 
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def save_to_file(name, score):
+    with open('Scoreboard.json', 'r') as file_read:
+        lis = json.load(file_read)
+    lis.append(name)
+    lis.append(score)
+    with open('Scoreboard.json', 'w') as file_write:
+        json.dump(lis, file_write)
+
+
+def show_scoreboard():
+    with open('Scoreboard.json', 'r') as file_load:
+        file_list = json.load(file_load)
+    name_lst = file_list[0::2]
+    score_list = file_list[1::2]
+    for i in range(len(name_lst)):
+        print(f"{name_lst[i]} has {score_list[i]} points")
+
+
 def menu():
-    choice = input("Menu:\n1 - Start Game\n2 - Exit\n")
+    choice = input("Menu:\n1 - Start Game\n2 - Exit\n3 - Scoreboard\n")
     if choice == '1':
         deck()
     elif choice == '2':
         input("Good bye!\nPress any button")
         sys.exit()
+    elif choice == '3':
+        show_scoreboard()
+        input("Press any button to continue... ")
+        menu()
     else:
         cls()
         menu()
 
 
 def deck():
-    player_cash = 100
     num_decks = ''
 
     while num_decks <= '0' or num_decks >= '5':
@@ -31,17 +54,19 @@ def deck():
     random.shuffle(game_deck)
     random.shuffle(game_deck)
     cls()
-    game(game_deck, player_cash)
+    game(game_deck, 100)
 
 
 def game(game_deck, player_cash):
     lose = 0
     bet = 0
+    player_name = input("Enter your name: ")
 
     while True:
         try:
             bet = int(input("Choose the bet(From 5 to 20): "))
             if 5 <= bet <= 20:
+                cls()
                 break
             else:
                 cls()
@@ -50,20 +75,21 @@ def game(game_deck, player_cash):
             continue
 
     dealer_hand = game_deck.pop(0)
-    print(f"Dealer's open hand:{dealer_hand}")
+    print(f"Dealer's open hand:{dealer_hand}\n")
     hand = game_deck[0] + game_deck[1]
-    print(f"Your cards: {game_deck[0]} and {game_deck[1]}")
+    print(f"Your cards: {game_deck[0]} and {game_deck[1]}\nTotal:{hand}")
     del game_deck[0:2]
 
     if hand == 21:
-        print("BlackJack! Now wait dealer.")
+        print("BlackJack! Now wait dealer.\n")
 
     elif hand == 22:
         hand = 12
         print(f"Your points:{hand}")
 
-    while True:
-        action = input("1 - Hit\n2 - Stop\n")
+    while hand != 21:
+        action = input("\n1 - Hit\n2 - Stop\n")
+        cls()
         if action == '1':
             hand += game_deck[0]
             if game_deck[0] == 11 and hand > 21:
@@ -74,7 +100,7 @@ def game(game_deck, player_cash):
             player_info = f"Your card:{game_deck[0]}\nYour points:{hand}"
             if hand > 21:
                 player_cash -= bet
-                print(f"{player_info}You bust!\nYour cash is:{player_cas}")
+                print(f"\n{player_info}You bust!\nYour cash is:{player_cash}")
                 lose = 1
                 break
             print(player_info)
@@ -87,9 +113,9 @@ def game(game_deck, player_cash):
             dealer_hand += game_deck[0]
             if game_deck[0] == 11 and dealer_hand >= 21:
                 dealer_hand -= 10
-            print(f"Dealer's card:{game_deck[0]}\nDealer's player_cash:{dealer_hand}")
+            print(f"Dealer's card:{game_deck[0]}\nDealer's score:{dealer_hand}\n")
             del game_deck[0]
-        result_info = f"Your player_cash:{hand}\nDealer's player_cash:{dealer_hand}"
+        result_info = f"Your score:{hand}\nDealer's score:{dealer_hand}\n"
         if hand < dealer_hand <= 21:
             player_cash -= bet
             print(result_info + f"\nYou Lose!\nNow your cash:{player_cash}")
@@ -98,19 +124,28 @@ def game(game_deck, player_cash):
             print(result_info + f"\nDraw!\nNow your cash:{player_cash}")
         elif dealer_hand < hand or dealer_hand > 21:
             player_cash += bet * 2
-            print(result_info + f"\nYou Win!\n Now your cash:{player_cash}")
+            print(result_info + f"\nYou Win!\nNow your cash:{player_cash}")
 
     if player_cash <= 0:
-        input("You lose all of your cash!\n Type any button to continue")
+        input("You lost all of your cash!\nType any button to continue")
         menu()
+
     while True:
-        retry = input("Want to play again?\n1 - Yes\n2 - Back to menu\n")
+        retry = input("\nWant to play again?\n1 - Yes\n2 - Back to menu\n")
         if retry == '1':
             cls()
             game(game_deck, player_cash)
         elif retry == '2':
             input(f"Thanks for playing!\nYour cash is {player_cash}\nGood luck!\nPress any button to continue")
+            save_to_file(player_name, player_cash)
             menu()
+
+
+file_check = pathlib.Path('Scoreboard.json')
+basic_list = ["Dima", "12", "Andrey", "97", "Steve", "150"]
+if not file_check.exists():
+    with open('Scoreboard.json', 'w') as f:
+        json.dump(basic_list, f)
 
 
 menu()
